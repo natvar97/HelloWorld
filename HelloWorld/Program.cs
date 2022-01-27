65989162f01ecc4205f3ec8684aac962335758eb
Indialone <prajapatinatavar21197@gmail.com>
2022-01-28 00:37:50 +0530

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            byte number = 255;
+            // checked throws an exception if overthrowing happens
+            checked
+            {
+                number = (byte)(number + 1);
+            }
+           
+            Console.WriteLine(number);
+
+
+        }
+    }
+}
