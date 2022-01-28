caffe63b099966b40ebedf5cbed6e39235acae43
Indialone <prajapatinatavar21197@gmail.com>
2022-01-29 00:16:40 +0530

    classes introducted

---
+++ HelloWorld/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine("My name is : " + firstName + " "  + lastName);
+        }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using HelloWorld.Math;
 
 namespace HelloWorld
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            const float Pi = 3.14f;\
+            var person = new Person();
+            person.firstName = "Urvashi";
+            person.lastName = "Prajapati";
+            person.Introduce();
+
+            var calculator = new Calculator();
+            var sum = calculator.Add(20, 10);
+            System.Console.WriteLine(sum);
+
         }
     }
 }
