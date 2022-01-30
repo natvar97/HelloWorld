e3a68177f6c1b5f6c529230ace2a303ff55a72c2
Indialone <prajapatinatavar21197@gmail.com>
2022-01-30 18:11:17 +0530

    excercise part 3 done

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using HelloWorld.Math;
+using System.Collections.Generic;
 using System;
 
 namespace HelloWorld
@@ -7,116 +8,186 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            // excercise - 1 : remainder
+            // excercise - 1
             /*
-            var count = 0;
-            for(var i = 0; i <= 100; i++)
-            {
-                if(i % 3 == 0 )
-                {
-                    count++;
-                }
-            }
-            Console.WriteLine("Counts: " + count);
-            */
-
-            // excercise - 2 : 
-            /*
-            var sum = 0;
+            var users = new List<string>();
             while(true)
             {
-                Console.Write("Enter a number or type \"ok\" to exit: ");
-                var input = Console.ReadLine();
-                if(input.ToLower() == "ok")
+                Console.Write("Enter Username: ");
+                var name = Console.ReadLine();
+                if(String.IsNullOrEmpty(name.Trim()))
                 {
-                    Console.WriteLine("sum of entered number: " + sum);
+                    if(users.Count == 1)
+                    {
+                        Console.WriteLine(users[0] + " liked your post.");
+                    } else if(users.Count == 2)
+                    {
+                        Console.WriteLine(users[0] + " and " + users[1] + " liked your post.");
+                    } else if(users.Count >= 3 )
+                    {
+                        Console.WriteLine(users[0] + ", " + users[1] + " and " + (users.Count - 2) + " others liked your post.");
+                    } else
+                    {
+                        Console.WriteLine("No one likes your post.");
+                    }
                     break;
                 }
+                users.Add(name);
+            }
+            */
+
+            // excercise - 2
+            /*
+            Console.Write("Enter your name: ");
+            var name = Console.ReadLine();
+            var namesArray = new string[name.Length];
+        
+            for(var i = 0; i < name.Length; i++)
+            {
+                namesArray[i] = name[i].ToString();
+            }
+
+            Array.Reverse(namesArray);
+
+            var newName = "";
+            
+            foreach(var value in namesArray)
+            {
+                newName += value;
+            }
+
+            Console.WriteLine(newName);
+            */
+
+            // excercise - 3
+
+            /*
+            Console.Write("Enter unique 5 numbers: ");
+
+            var numbers = new List<int>();
+
+            
+            while(true)
+            {
                 try
                 {
-                    sum += int.Parse(input);
-                } 
-                catch(Exception) 
+                    var number = int.Parse(Console.ReadLine());
+                    var isAlready = false;
+                    foreach(var value in numbers)
+                    {
+                        if(number == value)
+                        {
+                            isAlready = true;
+                            Console.WriteLine("You already entered " + number + " earlier, please enter all different numbers");
+                        }
+                    }
+                    
+                    if(!isAlready)
+                    {
+                        numbers.Add(number);
+                    }
+                    
+                    if (numbers.Count == 5)
+                    {
+                        break;
+                    }
+                } catch(Exception) {
+                    Console.WriteLine("Enter valid number");
+                    continue;
+                }
+            }
+
+            numbers.Sort();
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+            */
+
+            // excercise - 4
+
+            /*
+            var numbers = new List<int>();
+            Console.WriteLine("Type quit to exit program");
+            Console.WriteLine();
+
+            while(true)
+            {
+                Console.Write("Enter a number: ");
+                try
+                {
+                    var input = Console.ReadLine();
+                    if(input.ToLower().Equals("quit"))
+                    {
+                        break;
+                    }
+                    var number = int.Parse(input);
+                    numbers.Add(number);
+                } catch(Exception)
                 {
                     Console.WriteLine("You entered invalid number");
                     continue;
                 }
             }
-            */
 
-            // excercise - 3:
-            /*
-            Console.Write("Enter number: ");
-            var number = 0;
-            try
+            Console.WriteLine();
+            var newNumbers = new List<int>();
+            foreach(var number in numbers)
             {
-                number = int.Parse(Console.ReadLine());
-                var result = 1;
-                for (var i = number; i > 0; i--)
+                var isAlready = false;
+                foreach(var newNumber in newNumbers)
                 {
-                    result *= i;
+                    if(number == newNumber)
+                    {
+                        isAlready = true;
+                    }
                 }
-                Console.WriteLine(number + "! = " + result);
-            } catch (Exception)
+                if(!isAlready)
+                {
+                    newNumbers.Add(number);
+                }
+                
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Entered unique numbers");
+            foreach(var number in newNumbers)
             {
-                Console.WriteLine("You entered invalid number");
+                Console.WriteLine(number);
             }
             */
-
-            // excercise - 4:
-            /*
-            var random = new Random();
-            var randomNumber = random.Next(1, 10);
-            Console.WriteLine("Random Number: " + randomNumber);
-
-            for(var i = 0; i < 4; i++)
-            {
-                Console.Write("Enter a number between 1-10 : ");
-                try
-                {
-                    var number = int.Parse(Console.ReadLine());
-                    if(number == randomNumber)
-                    {
-                        Console.WriteLine("You won");
-                        break;
-                    }
-                    else
-                    {
-                        if(i < 3)
-                        {
-                            Console.WriteLine("You loose, make another try: remain trials: " + (3 - i));
-                        } else {
-                            Console.WriteLine("You lost the game.");
-                        }
-                    }
-                } catch (Exception)
-                {
-                    Console.WriteLine("You entered invalid number. remain trials: " + (3 - i));
-                    continue;
-                }
-            }
-           */
 
             // excercise - 5
-           
-            Console.Write("Enter multiple numbers divide by \',\' comma : ");
 
-            var numberSeries = Console.ReadLine();
-            var numbersStr = numberSeries.Split(',');
+            var numbers = new List<int>();
 
-
-            var max = 0;
-            foreach (var number in numbersStr)
+            while(true)
             {
-                var value = int.Parse(number);
-                if ( value > max)
+                Console.Write("Enter comma separated numbers greater than 5 numbers: ");
+                var inputNumbers = Console.ReadLine();
+               
+                var separedNumbers = inputNumbers.Split(',');
+              
+                var intNumbers = new int[separedNumbers.Length];
+                for (var i = 0; i < separedNumbers.Length; i++)
                 {
-                    max = value;
+                    intNumbers[i] = int.Parse(separedNumbers[i]);
                 }
+                numbers.AddRange(intNumbers);
+                if (numbers.Count >= 5)
+                {
+                    break;
+                }
+                numbers.Clear();
+                Console.WriteLine("Invalid list, Please try again");
             }
 
-            Console.WriteLine("maximum number : " + max);
-           
+            numbers.Sort();
+            for (var i = 0; i < 3 ; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
         }
     }
 }
